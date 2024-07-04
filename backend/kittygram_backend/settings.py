a9aa29c675db1a2eee2340094d7f6b95edd58973
Антон <frostik96@gmail.com>
2024-07-04 17:26:09 +0300

    backend/kittygram_backend/settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['84.201.142.188','localhost','127.0.0.1','kittygram-project.zapto.org']
 
